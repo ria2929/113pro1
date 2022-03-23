379fa36368d1a4d7e17282e5738df14280ebe079
ria2929 <97019184+ria2929@users.noreply.github.com>
2022-03-23 19:35:20 +1100

    Add files via upload

---
+++ 113.js
@@ -0,0 +1,30 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    
+
+}
+
+function draw(){
+    rect(30, 20, 55, 55);
+describe('pink rect with black outline in mid-right of canvas');
+rect(40, 20, 55, 55);
+describe('pink rect with black outline in mid-right of canvas');
+}
+circle(30, 30, 20);
+describe('white circle with black outline in mid of gray canvas');
+rect(30, 20, 55, 55, 20);
+describe(
+  'white rect with black outline and round edges in mid-right of canvas'
+);
+
+function take_snapshot(){
+    save('birthday_name.png');
+}
+
+
+
